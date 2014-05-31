ef3fc8bc30c978e9f3391d7dd74660403e354360
hnaohiro <q@hnaohiro.com>
2014-06-01 00:44:30 +0900

    Initial Commit

---
+++ bangohan-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bangohan-ios
+//
+//  Created by Hasegawa Naohiro on 2014/06/01.
+//  Copyright (c) 2014年 hnaohiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ bangohan-ios/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  bangohan-ios
+//
+//  Created by Hasegawa Naohiro on 2014/06/01.
+//  Copyright (c) 2014年 hnaohiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
